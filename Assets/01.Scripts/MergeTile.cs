d786de8afd1463df98720242fc06e337040aefa6
LKM0222 <python03@naver.com>
2024-02-10 14:29:47 +0900

    add : 타일Merge기능 추가 fix : 스크립트 관리폴더 추가, 스크립트 폴더로 이동

---
+++ Assets/01.Scripts/MergeTile.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MergeTile : MonoBehaviour
+{   
+    [SerializeField] GameObject cell;
+    [SerializeField] GameObject targetTile;
+    [SerializeField] Tile tile;
+
+    [SerializeField] Vector3 startPos;
+
+
+    private void OnMouseDown() {
+        //처음위치 저장
+        startPos = this.transform.position;
+    }
+
+    private void OnMouseDrag() {
+        //타일이 마우스를 따라다니게 설정.
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = new Vector3(mousePos.x, mousePos.y, this.transform.position.z);
+    }
+
+    private void OnMouseUp() {
+        //셀이 있는 경우
+        if(cell != null){
+            //타겟 타일이 있는 경우
+            if(targetTile != null){
+                //타겟 타일의 정보를 비교해야됨.
+                if(CompareTile(targetTile.GetComponent<MergeTile>().tile, tile)){ //조건이 맞다면
+                    this.transform.position = cell.transform.position; //위치를 옮기고
+                    tile.UpLevel(); //타일의 레벨 1 증가
+                    Destroy(targetTile); //타겟 타일 파괴
+                }
+                else{
+                    this.transform.position = startPos; //조건과 맞지 않다면 처음 자리로
+                }
+            }
+            else{ //타겟 타일이 없는경우엔 그냥 셀로만 이동
+                this.transform.position = cell.transform.position;
+            }
+        }
+        else{ //셀이 없는경우 이동할곳이 없음
+            //처음 시작 자리로
+            this.transform.position = startPos;
+        }
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        //타일이 셀과 부딛혔을땐 타일의 셀 정보에 셀을 저장.
+        if(other.tag == "Cell"){
+            cell = other.gameObject;
+        }
+        else if(other.tag == "Tile"){
+            targetTile = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        //타일이 셀에서 나갔을땐 cell정보 파괴
+        if(other.tag == "Cell"){
+            cell = null;
+        }
+        else if(other.tag == "Tile"){
+            targetTile = null;
+        }
+    }
+
+    bool CompareTile(Tile tile1, Tile tile2){
+        if(tile1.GetLevel() == tile2.GetLevel() && tile1.GetTileType() == tile2.GetTileType()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
